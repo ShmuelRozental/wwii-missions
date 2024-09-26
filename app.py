5703bed1473375e08ab860cada03509aeb3dfa97
ShmuelRozental <rozentalshmuel@gmail.com>
2024-09-26 17:57:24 +0300

    add routes for missions

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from config import Config
+from models.Mission import Mission
+from routes.missions import missions_bp
 from scripts.create_tables import create_tables
 from scripts.fetch_denormalized_data import fetch_denormalized_data
 from scripts.insert_data import insert_normalized_data
@@ -7,15 +9,12 @@ from scripts.insert_data import insert_normalized_data
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
-data = fetch_denormalized_data()
-create_tables()
-insert_normalized_data(data)
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Uncomment this if you need to initialize your database
+# data = fetch_denormalized_data()
+# create_tables()
+# insert_normalized_data(data)
+app.register_blueprint(missions_bp, url_prefix='/api/mission')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
